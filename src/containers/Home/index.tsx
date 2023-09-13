910b7f57d53470df10dba68f64ed805a6ee53d8e
yukun yang <yukkyang@gmail.com>
2023-09-13 23:51:39 +0800

    fix: bugfix and add some restriction

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,9 +1,10 @@
-import { Button, Input, Space, Tabs } from "antd";
+import { Input, Space, Tabs } from "antd";
 import { useEffect, useRef, useState } from "react";
 import { useMyContext } from "../../hooks/useContext";
 import AllowanceList from "../../components/AllowanceList";
-import styles from './index.module.scss'
+import styles from "./index.module.scss";
 import EditableValue from "../../components/EditableValue";
+import { SmileOutlined } from '@ant-design/icons';
 type TargetKey = React.MouseEvent | React.KeyboardEvent | string;
 
 const Home = () => {
@@ -19,7 +20,7 @@ const Home = () => {
       if (item?.key === key) {
         return {
           ...item,
-          name
+          name,
         };
       }
       return item;
@@ -29,14 +30,19 @@ const Home = () => {
       key,
       groups,
     });
-  }
+  };
 
   useEffect(() => {
     if (list) {
       setActiveKey(list.key);
       setItems(
         list?.groups?.map((item) => ({
-          label: <EditableValue value={item.name} onChange={(value) => onNameChange(item.key, value)} />,
+          label: (
+            <EditableValue
+              value={item.name}
+              onChange={(value) => onNameChange(item.key, value)}
+            />
+          ),
           key: item.key,
           children: <AllowanceList key={item.key} keyId={item.key} />,
         }))
@@ -56,15 +62,17 @@ const Home = () => {
     updateStorage("list", {
       ...list,
       key,
-      groups: [...list?.groups ?? [], { key, name: value, list: [] }],
+      groups: [...(list?.groups ?? []), { key, name: value, list: [] }],
     });
-    setValue('')
+    setValue("");
   };
 
   const remove = (targetKey: TargetKey) => {
+    const groups = list?.groups.filter((item: any) => item.key !== targetKey);
     updateStorage("list", {
       ...list,
-      groups: list?.groups.filter((item: any) => item.key !== targetKey),
+      key: list?.key === targetKey ? groups?.[0]?.key : list?.key,
+      groups,
     });
   };
 
@@ -79,26 +87,42 @@ const Home = () => {
     }
   };
 
+  const Search = (
+    <Space.Compact className={styles.search} style={{ width: "100%" }}>
+      <Input.Search
+        placeholder="Type a group name"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onSearch={add}
+        enterButton={"Add"}
+      />
+    </Space.Compact>
+  );
+
   return (
     <div className={styles.home}>
-      <Space.Compact className={styles.search} style={{ width: "100%" }}>
-        <Input
-          placeholder="Input a group name to add"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
+      {items.length === 0 && Search}
+      {items.length > 0 ? (
+        <Tabs
+          tabBarExtraContent={{
+            right: Search,
+          }}
+          hideAdd
+          type="editable-card"
+          onChange={onChange}
+          activeKey={activeKey}
+          onEdit={onEdit}
+          items={items}
         />
-        <Button type="primary" onClick={add} disabled={!value}>
-          Add
-        </Button>
-      </Space.Compact>
-      <Tabs
-        hideAdd
-        type="editable-card"
-        onChange={onChange}
-        activeKey={activeKey}
-        onEdit={onEdit}
-        items={items}
-      />
+      ) : (
+        <div>
+          <div style={{ textAlign: "center" }}>
+            <SmileOutlined style={{ fontSize: 20 }} rev={undefined} />
+            <p>Add a group to start ordering</p>
+            <p>Have a nice day : )</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
